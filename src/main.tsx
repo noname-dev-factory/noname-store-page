3503a45fdc72501318fe05b9f0a808bc4c8aac41
Christian Arevalo <tiancris200@gmail.com>
2022-12-07 12:16:21 -0500

    feat: added Home Screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Products from './components/Products'
 import './index.css'
-
-const test = false
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {!test
-      ? <App />
-      : <Products />}
+    <App />
   </React.StrictMode>
 )
