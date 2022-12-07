af1ff4ddfb024e2f06da2c52b9419dd120748856
Christian Arevalo <tiancris200@gmail.com>
2022-12-07 12:07:36 -0500

    feat: Added home screen

---
+++ src/components/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import './styles/HomeScreen.css'
+
+
+export default function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <div className="homeScreen__wrapper place_center">
+                <h1>noname</h1>
+            </div>
+        </div>
+    )
+}
